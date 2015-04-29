93d5f16f722f0b48df2aea697509a3ab465ec9a4
unknown <n@RED.core.alio.me>
2015-04-29 08:12:44 -0400

    removing old tentacle module

--- DSC/Modules/OctopusDeploy/DSCResources/OctopusTentacle/OctopusTentacle.schema.psm1
+++
@@ -1,9 +0,0 @@
-
-$ApplicationName = 'Octopus Deploy Tentacle'
-$ServiceBaseName = 'OctopusDeploy'
-$DefaultInstallPath = "${env:ProgramFiles}\Octopus Deploy\Tentacle"
-$Tentacle = 'tentacle.exe'
-
-Get-ChildItem -Path $PSScriptRoot\*.ps1 | Foreach-Object{ . $_.FullName }
-
-Export-ModuleMember -function *
